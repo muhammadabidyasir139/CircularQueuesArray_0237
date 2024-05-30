e01eee2ae975dc2a1c21cc243d8f1c13b60b52a3
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-05-30 15:41:06 +0700

    membuat flow pilihan

--- circullarqueues.cpp
+++ circullarqueues.cpp
@@ -104,8 +104,27 @@ int main() {
         try {
             cout << "Menu" << endl;
             cout << "1. Implement insert operation" << endl;
-            cout << "1. Implement delete operation" << endl;
-            cout << "1. Display Values" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1 -4): " << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch () {
+                case '1' : {
+                    q.insert();
+                    break;
+                }
+                case '2' : {
+                    q.remove();
+                    break;
+                }
+                case '3' : {
+                    q.display();
+                    break;
+                }
+            }
         }
     }
     
