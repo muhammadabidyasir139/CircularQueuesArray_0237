df5b8b0a23e6c85064b467886aa82100df6e34fd
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-05-30 15:15:59 +0700

    membuat logic untuk display

--- circullarqueues.cpp
+++ circullarqueues.cpp
@@ -56,11 +56,22 @@ class Queues {
             REAR = -1;
         }
         else {
-            //jika elemen yang dihapus beada di posisi terakhir array, kembali ke awal
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
             if (FRONT == max -1)
                 FRONT = 0;
             else   
                 FRONT = FRONT + 1;
+        }
+     }
+
+     void display() {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        // Cek apakah antrian kosong 
+        if(FRONT_Position == -1) {
+            cout << "Queue is empty\n";
+            return;
         }
      }
 };
